ad01b26bb32739db9d534d994b840539eba85fde
Cole Severson <cseverso@uoregon.edu>
2019-07-13 01:16:47 -0700

    Added working hello world test program

--- Brainfunge.py
+++ Brainfunge.py
@@ -5,7 +5,7 @@ import sys
 
 #Input and Memory
 sourceInput = []
-tape = [0] * (sys.maxsize / 2)
+tape = [0] * 1024
 #Constant Directions
 RIGHT = (0, 1)
 LEFT = (0, -1)
@@ -15,10 +15,44 @@ MAXINT = sys.maxsize
 
 direction = RIGHT
 pos = (0, 0)
+pointer = 0
 
 for line in fileinput.input():
-    sourceInput.append(line.rstrip("\r\n").split(' '))
+    sourceInput.append(line.rstrip("\r\n"))
 
+while(True):
+    if(pos[0] >= len(sourceInput) or pos[1] >= len(sourceInput[pos[0]])
+            or pos[0] < 0 or pos[1] < 0):
+        break
+    symbol = sourceInput[pos[0]][pos[1]]
+    #tape options
+    if symbol == '+':
+        tape[pointer] += 1
+    elif symbol == '-':
+        tape[pointer] -= 1
+    #pointer options
+    elif symbol == '/':
+        pointer += 1
+    elif symbol == '\\':
+        pointer -= 1
+    #Directional options
+    elif symbol == ">":
+        direction = RIGHT
+    elif symbol == "<":
+        direction = LEFT
+    elif symbol == "^":
+        direction = UP
+    elif symbol == "v":
+        direction = DOWN
+    #Print options
+    elif symbol == 'c': #handling newline
+        print(chr(tape[pointer]), end = "")
+    elif symbol == 'i':
+        print(int(tape[pointer]), end = "")
+    #TODO add conditionals 
+
+    #Execute a move. This is also the default option for whitespace or unrecognized characters
+    pos = (pos[0] + direction[0], pos[1] + direction[1])
 #Setup finished
 
 
